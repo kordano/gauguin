91203595025886c008c149a00f6b59c2a1782f41
kordano <konrad.kuehne@rocketmail.com>
2014-12-12 13:26:39 +0100

    add force directed graph

--- src/gaugin/core.cljs
+++ src/gaugin/core.cljs
@@ -6,7 +6,7 @@
 
 (enable-console-print!)
 
-(fw/start {
+#_jjjjjjjjjjjjjjjkkkkkkkkk(fw/start {
   ;; configure a websocket url if yor are using your own server
   ;; :websocket-url "ws://localhost:3449/figwheel-ws"
 
@@ -28,99 +28,125 @@
 
 (def tree-data
   (clj->js
-   {:name "s1"
+   {:name "p1"
     :parent nil
-    :children [{:name "r1"
-                :parent "s1"
-                :children [{:name "r2"
-                            :parent "r1"
-                            :children [{:name "r6"
-                                        :parent "r2"}
-                                       {:name "r7"
-                                        :parent "r2"}
-                                       {:name "r8"
-                                        :parent "r2"}
-                                       {:name "r9"
-                                        :parent "r2"}
-                                       {:name "r10"
-                                        :parent "r2"}
-                                       ]}
-                           {:name "r3"
-                            :parent "r1"}]}
-               {:name "r4"
-                :parent "s1"}
-               {:name "r5"
-                :parent "s1"}
-               {:name "r11"
-                :parent "s1"}
-               {:name "r12"
-                :parent "s1"}
-               {:name "r13"
-                :parent "s1"}
-               {:name "r14"
-                :parent "s1"
-                :children [
-                          {:name "r15"
-                            :parent "r14"}
-                          {:name "r16"
-                            :parent "r14"}
-                          {:name "r17"
-                            :parent "r14"}
-                          {:name "r18"
-                            :parent "r14"}
-                          {:name "r19"
-                            :parent "r14"}
-                          {:name "r20"
-                            :parent "r14"}
-                           ]}
-               ]}))
+    :children [{:name "u1"
+                :parent "p1"}
+               {:name "p2"
+                :parent "p1"
+                :children [{:name "p4"
+                            :parent "p2"}
+                           {:name "p5"
+                            :parent "p5"}]}
+               {:name "p3"
+                :parent "p1"}
+               {:name "p4"
+                :parent "p1"}]}))
 
-(def width 2000 #_(.. js/window -screen -availWidth))
-(def height 2000 #_(.. js/window -screen -availHeight))
+(def graph-data
+  (clj->js
+   {:nodes [{:name "p1" :group 0}
+            {:name "p2" :group 0}
+            {:name "p3" :group 0}
+            {:name "p4" :group 0}
+            {:name "p5" :group 0}]
+    :links [{:source 1 :target 0 :value 1}
+            {:source 2 :target 0 :value 1}
+            {:source 3 :target 1 :value 1}
+            {:source 4 :target 1 :value 1}]}))
 
-(def tree (-> d3 .-layout (.tree) (.size [(- height 50) width])))
-
-(def diagonal (-> d3 .-svg .diagonal (.projection (fn [d] (clj->js [(* 0.6 (.-x d)) (* 0.6 (.-y d))])))))
-
-(def svg (-> d3 (.select "#the-canvas")
-      (.attr {:width width :height height})
-      (.append "g")
-      (.attr {:transform (str "translate(" (/ width 4) ",20)")})))
-
-(def nodes (.nodes tree tree-data))
-
-(def links (.links tree nodes))
-
-(def link (-> svg
+(defn draw-reingold
+  "doc-string"
+  [data]
+  (let [width 1080
+        height 920
+        tree (-> d3 .-layout (.tree) (.size [(- height 50) width]))
+        diagonal (-> d3 .-svg .diagonal (.projection (fn [d] (clj->js [(* 0.6 (.-x d)) (* 0.6 (.-y d))]))))
+        svg (-> d3 (.select "#the-canvas")
+                (.attr {:width width :height height})
+                (.append "g")
+                (.attr {:transform (str "translate(" (/ width 4) ",20)")}))
+        nodes (.nodes tree data)
+        links (.links tree nodes)
+        link (-> svg
               (.selectAll "path.link")
               (.data links)
               (.enter)
               (.append "path")
               (.attr {:class "link"
-                      :d diagonal})))
-
-(def node (-> svg
+                      :d diagonal}))
+        node (-> svg
               (.selectAll "g.node")
               (.data nodes)
               (.enter)
               (.append "g")
               (.attr {:class "node"
-                      :transform (fn [d] (str "translate(" (* 0.6 (.-x d)) "," (* 0.6 (.-y d)) ")"))})))
+                      :transform (fn [d] (str "translate(" (* 0.6 (.-x d)) "," (* 0.6 (.-y d)) ")"))}))]
+    (do
+      (-> node
+          (.append "circle")
+          (.attr {:r 4.5}))
+      (-> node
+          (.append "text")
+          (.attr {:dx 3
+                  :dy #(if (.-children %) -8 8)
+                  :text-anchor #(if (.-children %) "end" "start")})
+          (.style {:font-size "10px"})
+          (.text #(.-name %)))
+      (-> d3
+          (.select (.-frameElement js/self))
+          (.style {:height (str (- height 50) "px")
+                   :width (str width "px")})))))
 
-(-> node
-    (.append "circle")
-    (.attr {:r 4.5}))
 
-(-> node
-    (.append "text")
-    (.attr {:dx 3
-            :dy #(if (.-children %) -8 8)
-            :text-anchor #(if (.-children %) "end" "start")})
-    (.style {:font-size "10px"})
-    (.text #(.-name %)))
+(defn draw-fdg
+  "Draw force-directed graph"
+  [data]
+  (let [width 960
+        height 500
+        color (-> d3 .-scale .category10)
+        force (-> d3 .-layout (.force) (.charge -500) (.linkDistance 100) (.size [width height]))
+        svg (-> d3
+                (.select "#the-canvas-2")
+                (.attr {:width width
+                        :height height}))]
+    (-> force
+        (.nodes (.-nodes data))
+        (.links (.-links data))
+        .start)
+    (let [link (-> svg
+                   (.selectAll ".link")
+                   (.data (.-links data))
+                   (.enter)
+                   (.append "line")
+                   (.attr {:class "link"})
+                   (.style {:stroke-with (fn [d] (.sqrt js/Math (.-value d)))}))
+          node (-> svg
+                   (.selectAll ".node")
+                   (.data (.-nodes data))
+                   (.enter)
+                   (.append "circle")
+                   (.attr {:class "node"
+                           :r "5"})
+                   (.style {:fill (fn [d] (color (.-group d)))})
+                   (.call (.-drag force))
+                   )]
+      (do
+        (-> node (.append "title") (.text (fn [d] (.-name d))))
+        (-> force
+            (.on "tick"
+                 (fn []
+                   (-> link
+                       (.attr
+                        {:x1 #(-> % .-source .-x)
+                         :y1 #(-> % .-source .-y)
+                         :x2 #(-> % .-target .-x)
+                         :y2 #(-> % .-target .-y)}))
+                   (-> node
+                       (.attr {:cx #(.-x %)
+                               :cy #(.-y %)})))))))))
 
 
-(-> d3
-    (.select (.-frameElement js/self))
-    (.style {:height (str (- height 50) "px")
-             :width (str width "px")}))
+(draw-reingold tree-data)
+
+(draw-fdg graph-data)
